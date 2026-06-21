1f583b625a25fb1b33d8014db8de5431a769778b
Aygian-GH <aaryang9285@gmail.com>
2026-06-21 23:47:02 +0530

    DB UPDATES 1

--- PlateCheck/settings.py
+++ PlateCheck/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
 import os
 import dotenv
+import dj_database_url
+from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,11 +85,12 @@ WSGI_APPLICATION = 'PlateCheck.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 
